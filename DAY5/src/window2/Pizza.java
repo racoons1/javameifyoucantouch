acf66ee7a6829865817bec0a7fd9078d3b81a0fd
racoons1 <46391638+racoons1@users.noreply.github.com>
2019-01-05 14:50:55 +0900

    Add files via upload

--- DAY5/src/window2/Pizza.java
+++ DAY5/src/window2/Pizza.java
@@ -1,6 +1,7 @@
 package window2;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 
 import java.awt.FlowLayout;
@@ -9,6 +10,8 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.Color;
 
 public class Pizza {
 
@@ -22,10 +25,12 @@ public class Pizza {
 	int bulgoki = 0;
 	
 	JTextArea list;
+	private JLabel lblWow;
+	private JButton btnNewButton;
 		
 	public Pizza() {
 		f = new JFrame();
-		f.setSize(350, 200);
+		f.setSize(350, 260);
 		top = new JLabel("<<<<<<<<<<<<자바피자에 오신걸 환영>>>>>>>>>>>>");
 		count = new JLabel("개수");
 		b1 = new JButton("콤보피자");
@@ -34,18 +39,34 @@ public class Pizza {
 		total = new JTextField(20);
 
 		FlowLayout flow = new FlowLayout();
-		f.setLayout(flow);
+		f.getContentPane().setLayout(flow);
 		list = new JTextArea(3, 30);
 		
 		
 		
-		f.add(top);
-		f.add(b1);
-		f.add(b2);
-		f.add(b3);
-		f.add(count);
-		f.add(total);
-		f.add(list);
+		f.getContentPane().add(top);
+		f.getContentPane().add(b1);
+		f.getContentPane().add(b2);
+		f.getContentPane().add(b3);
+		f.getContentPane().add(count);
+		f.getContentPane().add(total);
+		f.getContentPane().add(list);
+		
+		btnNewButton = new JButton("\uB098\uB97C \uB20C\uB7EC\uC694");
+		btnNewButton.setFont(new Font("굴림", Font.BOLD, 12));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "나를 정말 누르셨군요.");
+				
+				
+			}
+		});
+		
+		lblWow = new JLabel("\uCD1D\uACC4");
+		lblWow.setForeground(Color.RED);
+		lblWow.setFont(new Font("굴림", Font.BOLD, 14));
+		f.getContentPane().add(lblWow);
+		f.getContentPane().add(btnNewButton);
 				
 
 		b1.addActionListener(new ActionListener() {
